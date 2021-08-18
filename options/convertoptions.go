c0cfc2d8a6197784d5a19b1aef0483b366e6a42a
sujames <surarerk.boonjit@gmail.com>
2021-08-18 18:12:54 +0700

    Add convex options

---
+++ options/convertoptions.go
@@ -0,0 +1,33 @@
+package options
+
+import (
+	"github.com/sujamess/convex/currency"
+	"github.com/sujamess/convex/lang"
+)
+
+type ConvertOptions struct {
+	Language     *lang.Lang
+	Currency     *currency.Currency
+	WithCurrency *bool
+}
+
+func MergeConvertOptions(opts ...*ConvertOptions) *ConvertOptions {
+	defaultLanguage, defaultCurrency, defaultWithCurrency := lang.AmericanEnglish, currency.USD, false
+	cOpts := &ConvertOptions{&defaultLanguage, &defaultCurrency, &defaultWithCurrency}
+
+	for _, co := range opts {
+		if co.Language != nil {
+			cOpts.Language = co.Language
+		}
+
+		if co.Currency != nil {
+			cOpts.Currency = co.Currency
+		}
+
+		if co.WithCurrency != nil {
+			cOpts.WithCurrency = co.WithCurrency
+		}
+	}
+
+	return cOpts
+}
